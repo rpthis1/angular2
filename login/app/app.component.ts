cd55ccee275aa3c7d47d2b4f29f969eae930e072
rpthis1 <mprado85@gmail.com>
2016-04-14 17:30:21 -0700

    adding login

---
+++ login/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+import { FORM_PROVIDERS,FormBuilder, Validators } from 'angular2/common';
+import {bootstrap} from 'angular2/platform/browser';
+//import {ControlMessages} from './control-messages.component';
+
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 99 App</h1>'
+})
+export class AppComponent { }
